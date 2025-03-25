74d32253221eb2fbe96ca562e274d69a5e2eba6c
Carlos Humberto Brasileiro Toscano <humbertobtoscano@gmail.com>
2025-03-25 11:50:55 -0300

    primeiro commit

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,11 +1,22 @@
 import Banner from "components/Banner";
 import Card from "components/Card";
 import Titulo from "components/Titulo";
-import videos from "json/db.json";
 import styles from "./Inicio.module.css";
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 
 function Inicio() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://my-json-server.typicode.com/humbertobrasileiro/cinetag-api/videos"
+    )
+      .then((resposta) => resposta.json())
+      .then((dados) => {
+        setVideos(dados);
+      });
+  }, []);
+
   return (
     <Fragment>
       <Banner imagem="home" />
